36a23e0bfe0300be88fe8b7baa6f99d656242727
Vipin Kumar <vipin.mca17.du@gmail.com>
2018-04-26 20:55:45 +0530

    Add files via upload

--- doublyLinkedList.py
+++ doublyLinkedList.py
@@ -1,17 +1,54 @@
+'''
+Program         : Doubly Linked list Class
+Author(s)       : 1. Smrity Chaudhary
+                  2. Vipin Kumar
+'''
+
+# Import necessary libraries.
 from os import listdir
 from os.path import isfile,join
 
+# -----------------------------------------------------------------------------
+# Class Definition
 class Node:
+    '''
+    Node: Node class.
+    '''
+
     def __init__(self,data):
+        '''
+        Objective: To initialize data members of object Node
+        Input Parameters:
+            self (implicit parameter) - object of type Node
+            data - int
+        Return Value: None
+        '''
         self.data = data
         self.next = None
         self.prev = None
 
 class Doublylinkedlist:
+    '''
+    DoublyLinkedList Class Implementation.
+    '''
     def __init__(self):
+        '''
+        Objective: To initialize data members of object DoublyLinkedList
+        Input Parameters:
+            self (implicit parameter) - object of type MyDate
+        Return Value: None
+        '''
         self.head = None
 
+
     def insert(self,newData):
+        '''
+        Objective: To insert data at end into doubly linked list.
+        Input Parameters:
+            self (implicit parameter) - object of type MyDate
+            newData - string type, path for song.
+        Return Value: None
+        '''
         newNode = Node(newData)
         if self.head is None:
             self.head = newNode
@@ -31,6 +68,12 @@ class Doublylinkedlist:
 
 
     def display(self):
+        '''
+        Objective: To display the contents of linked list.
+        Input Parameters:
+            self (implicit parameter) - object of type MyDate
+        Return Value: None
+        '''
         temp = self.head
         while(temp is not self.head.prev):
             print(temp.data," ")
@@ -39,11 +82,23 @@ class Doublylinkedlist:
 
 
     def getNext(self):
+        '''
+        Objective: Get next value from linked list.
+        Input Parameters:
+            self (implicit parameter) - object of type MyDate
+        Return Value: Value of head.next.
+        '''
         self.head = self.head.next
         return self.head.data
 
 
     def getPrev(self):
+        '''
+        Objective: Get previous value from linked list.
+        Input Parameters:
+            self (implicit parameter) - object of type MyDate
+        Return Value: Value of head.prev.
+        '''
         self.head = self.head.prev
         return self.head.data
 
